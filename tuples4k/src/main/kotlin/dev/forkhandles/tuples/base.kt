12b7a484d731157b6fbc2cd6ab82849de846e8ac
Nat Pryce <sw@natpryce.com>
2020-07-01 17:23:56 +0100

    Tuples of up to eight elements Code generation from size four upwards Operations to add values to tuples, add tuples, and convert tuples to lists

---
+++ tuples4k/src/main/kotlin/dev/forkhandles/tuples/base.kt
@@ -0,0 +1,11 @@
+package dev.forkhandles.tuples
+
+typealias Tuple2<T1, T2> = Pair<T1,T2>
+val <T1,T2> Tuple2<T1, T2>.val1: T1 get() = first
+val <T1,T2> Tuple2<T1, T2>.val2: T2 get() = second
+
+typealias Tuple3<T1, T2, T3> = Triple<T1,T2,T3>
+val <T1,T2,T3> Triple<T1,T2,T3>.val1: T1 get() = first
+val <T1,T2,T3> Triple<T1,T2,T3>.val2: T2 get() = second
+val <T1,T2,T3> Triple<T1,T2,T3>.val3: T3 get() = third
+
--- tuples4k/src/test/kotlin/dev/forkhandles/tuples/PlaceholderTest.kt
+++ tuples4k/src/test/kotlin/dev/forkhandles/tuples/PlaceholderTest.kt
@@ -4,9 +4,10 @@ import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import org.junit.jupiter.api.Test
 
-class PlaceholderTest {
+class TupleExampleTest {
     @Test
-    fun placeholder() {
-        assertThat(false, equalTo(false))
+    fun `just a little example`() {
+        assertThat(tuple(1, 2, 3, 4, 5) + tuple(6, 7, 8), equalTo(tuple(1, 2, 3, 4, 5, 6, 7, 8)))
+        assertThat(tuple(1,2,3,4,5).toList(), equalTo(listOf(1,2,3,4,5)))
     }
 }
