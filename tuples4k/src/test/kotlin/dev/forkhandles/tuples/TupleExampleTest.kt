6041ae16b273f9dfaf5ac97bab8320a2f0dcf048
Nat Pryce <sw@natpryce.com>
2020-07-02 10:05:05 +0100

    Convenient operations for dealing with null elements

--- tuples4k/src/test/kotlin/dev/forkhandles/tuples/TupleExampleTest.kt
+++ tuples4k/src/test/kotlin/dev/forkhandles/tuples/TupleExampleTest.kt
@@ -1,13 +1,26 @@
 package dev.forkhandles.tuples
 
+import com.natpryce.hamkrest.absent
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import org.junit.jupiter.api.Test
 
 class TupleExampleTest {
     @Test
-    fun `just a little example`() {
+    fun `appending tuples`() {
         assertThat(tuple(1, 2, 3, 4, 5) + tuple(6, 7, 8), equalTo(tuple(1, 2, 3, 4, 5, 6, 7, 8)))
+    }
+    
+    @Test
+    fun `to lists`() {
         assertThat(tuple(1,2,3,4,5).toList(), equalTo(listOf(1,2,3,4,5)))
+    }
+    
+    @Test
+    fun `null to non-null`() {
+        val t : Tuple4<Int?,Int?,Int?,Int?> = tuple(1,2,3,4)
+        
+        assertThat(t.allNonNull(), equalTo(t))
+        assertThat(t.copy(val2=null).allNonNull(), absent())
     }
 }
