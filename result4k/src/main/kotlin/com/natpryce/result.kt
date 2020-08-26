cd41a6f25fa047d08a479e86e7d39d8afd1c2a03
David Denton <denton.david@gmail.com>
2020-08-26 17:45:05 +0100

    clean up deprecation

--- result4k/src/main/kotlin/com/natpryce/result.kt
+++ result4k/src/main/kotlin/com/natpryce/result.kt
@@ -5,13 +5,13 @@ import dev.forkhandles.result4k.Result as ResultNew
 import dev.forkhandles.result4k.Success as Success1
 import dev.forkhandles.result4k.resultFrom as resultFromNew
 
-@Deprecated("Repackaged", ReplaceWith("dev.forkhandles.result4k.Result<A, B>"))
+@Deprecated("Repackaged", ReplaceWith("dev.forkhandles.result4k.Result"))
 typealias Result<A, B> = ResultNew<A, B>
 
-@Deprecated("Repackaged", ReplaceWith("dev.forkhandles.result4k.Success<A>"))
+@Deprecated("Repackaged", ReplaceWith("dev.forkhandles.result4k.Success"))
 typealias Success<A> = Success1<A>
 
-@Deprecated("Repackaged", ReplaceWith("dev.forkhandles.result4k.Failure<A>"))
+@Deprecated("Repackaged", ReplaceWith("dev.forkhandles.result4k.Failure"))
 typealias Failure<A> = FailureNew<A>
 
 @Deprecated("Repackaged", ReplaceWith("dev.forkhandles.result4k.resultFrom()"))
