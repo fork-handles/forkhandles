19830ff491f32331fa0b70ed7740f76683d8adec
David Denton <denton.david@gmail.com>
2021-04-22 20:40:25 +0100

    don't use bintray

--- release.sh
+++ release.sh
@@ -7,14 +7,14 @@ set -o nounset
 
 NEW_VERSION=$1
 
-BINTRAY_VERSION=$(curl -s https://bintray.com/api/v1/packages/fork-handles/maven/forkhandles-bom/versions/_latest | tools/jq -r .name)
+OLD_VERSION=$(cat version.json | tools/jq -r .forkhandles.version)
 
 git stash
 
-echo Upgrade from "$BINTRAY_VERSION" to "$NEW_VERSION"
+echo Upgrade from "$OLD_VERSION" to "$NEW_VERSION"
 
-find . -name "*.md" | grep -v "CHANGELOG" | xargs -I '{}' sed -i '' s/"$BINTRAY_VERSION"/"$NEW_VERSION"/g '{}'
-sed -i '' s/"$BINTRAY_VERSION"/"$NEW_VERSION"/g version.json
+find . -name "*.md" | grep -v "CHANGELOG" | xargs -I '{}' sed -i '' s/"$OLD_VERSION"/"$NEW_VERSION"/g '{}'
+sed -i '' s/"$OLD_VERSION"/"$NEW_VERSION"/g version.json
 
 git commit -am"Release $NEW_VERSION"
 git push
