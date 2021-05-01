fac7a41db44034a3f3c046a1d781b85d2e8da844
David Denton <denton.david@gmail.com>
2021-05-01 13:56:18 +0100

    remove jacoco

--- build-ci.sh
+++ build-ci.sh
@@ -5,5 +5,7 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-./gradlew -i check jacocoRootReport --build-cache
-bash <(curl -s https://codecov.io/bash)
+./gradlew check --build-cache
+# TURN OFF JACOCO UNTIL https://github.com/jacoco/jacoco/issues/1086 is FIXED
+#./gradlew check jacocoRootReport --build-cache
+#bash <(curl -s https://codecov.io/bash)
