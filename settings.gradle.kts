4a88933d1718d7cc4c345255e30caa72e4513108
David Denton <denton.david@gmail.com>
2021-09-17 09:13:34 +0100

    upgrade refreshVersions

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("de.fayard.refreshVersions").version("0.21.0")
+}
+
+refreshVersions {
+    enableBuildSrcLibs()
+
+    rejectVersionIf {
+        candidate.stabilityLevel.isLessStableThan(current.stabilityLevel)
+    }
+}
+
+rootProject.name = "forkhandles"
+
+include("forkhandles-bom")
+
+include("bunting4k")
+include("fabrikate4k")
+include("parser4k")
+include("partial4k")
+include("result4k")
+include("time4k")
+include("tuples4k")
+include("values4k")
