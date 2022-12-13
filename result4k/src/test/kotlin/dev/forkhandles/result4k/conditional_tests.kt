f6bfebac3d19e44a18384f64d3ba2676ba3740d2
Andrew O'Hara <ohara.andrew314@gmail.com>
2022-12-13 17:52:06 -0500

    improve result4k conditional_tests coverage

--- result4k/src/test/kotlin/dev/forkhandles/result4k/conditional_tests.kt
+++ result4k/src/test/kotlin/dev/forkhandles/result4k/conditional_tests.kt
@@ -14,14 +14,14 @@ class RecoverIfTests {
 
     @Test
     fun `recover a failure when condition is met`() {
-        val r = Failure(1).recoverIf({ true }, { "a" })
+        val r = Failure(1).recoverIf({ it == 1 }, { "a" })
 
         assertEquals(Success("a"), r)
     }
 
     @Test
     fun `don't recover a failure when condition is not met`() {
-        val r = Failure(1).recoverIf({ false }, { "a" })
+        val r = Failure(1).recoverIf({ it == 2 }, { "a" })
 
         assertEquals(Failure(1), r)
     }
@@ -38,14 +38,14 @@ class FailIfTests {
 
     @Test
     fun `fail a success when condition is met`() {
-        val r = Success("a").failIf({ true }, { 1 })
+        val r = Success("a").failIf({ it == "a" }, { 1 })
 
         assertEquals(Failure(1), r)
     }
 
     @Test
     fun `don't fail a success when condition is not met`() {
-        val r = Success("a").failIf({ false }, { 1 })
+        val r = Success("a").failIf({ it == "b" }, { 1 })
 
         assertEquals(Success("a"), r)
     }
