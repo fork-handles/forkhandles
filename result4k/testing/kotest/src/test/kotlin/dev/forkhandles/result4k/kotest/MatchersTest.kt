40fd75f5fb57dc761a42932400c8bc2b343fce24
albertlatacz <albert.latacz@gmail.com>
2023-03-09 22:27:56 +0000

    improved kotest matchers assertions

--- result4k/testing/kotest/src/test/kotlin/dev/forkhandles/result4k/kotest/MatchersTest.kt
+++ result4k/testing/kotest/src/test/kotlin/dev/forkhandles/result4k/kotest/MatchersTest.kt
@@ -5,49 +5,83 @@ import com.natpryce.hamkrest.equalTo
 import com.natpryce.hamkrest.present
 import dev.forkhandles.result4k.Failure
 import dev.forkhandles.result4k.Success
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 
 class MatchersTest {
 
     @Test
-    fun `should fail with correct message on Failure when expecting Success`() {
-        val expectedValue = "Test failed"
-        val result = Failure(expectedValue)
+    fun `should correctly assert when Success`() {
+        val actualValue = "Successful"
+        val actualResult = Success(actualValue)
 
-        expectsAssertionError("Failure(reason=Test failed) should be Success") {
-            result.shouldBeSuccess()
+        assertDoesNotThrow { actualResult.shouldBeSuccess() }
+        assertDoesNotThrow { actualResult.shouldBeSuccess { assertEquals(actualValue, it) } }
+        assertDoesNotThrow { actualResult.shouldBeSuccess(actualValue) }
+    }
+
+    @Test
+    fun `should correctly assert when Failure`() {
+        val actualValue = "Failed"
+        val actualResult = Failure(actualValue)
+
+        assertDoesNotThrow { actualResult.shouldBeFailure() }
+        assertDoesNotThrow { actualResult.shouldBeFailure { assertEquals(actualValue, it) } }
+        assertDoesNotThrow { actualResult.shouldBeFailure(actualValue) }
+    }
+
+    @Test
+    fun `should correctly assert when Success but expecting Failure`() {
+        val actualValue = "Test successful"
+        val actualResult = Success(actualValue)
+
+        throwsAssertionError("Success(value=Test successful) should be Failure") {
+            actualResult.shouldBeFailure()
         }
 
-        expectsAssertionError("Failure(reason=Test failed) should be Success") {
-            result.shouldBeSuccess { }
+        throwsAssertionError("Success(value=Test successful) should be Failure") {
+            actualResult.shouldBeFailure { }
         }
 
-        expectsAssertionError("Failure(reason=Test failed) should be Success(value=Test failed)") {
-            result.shouldBeSuccess(expectedValue)
+        throwsAssertionError("Success(value=Test successful) should be Failure(reason=Test successful)") {
+            actualResult.shouldBeFailure(actualValue)
         }
     }
 
     @Test
-    fun `should fail with correct message on Success when expecting Failure`() {
-        val expectedValue = "Test successful"
-        val result = Success(expectedValue)
+    fun `should correctly assert when Failure but expecting Success`() {
+        val actualValue = "Test failed"
+        val actualResult = Failure(actualValue)
 
-        expectsAssertionError("Success(value=Test successful) should be Failure") {
-            result.shouldBeFailure()
+        throwsAssertionError("Failure(reason=Test failed) should be Success") {
+            actualResult.shouldBeSuccess()
         }
 
-        expectsAssertionError("Success(value=Test successful) should be Failure") {
-            result.shouldBeFailure { }
+        throwsAssertionError("Failure(reason=Test failed) should be Success") {
+            actualResult.shouldBeSuccess { }
         }
 
-        expectsAssertionError("Success(value=Test successful) should be Failure(reason=Test successful)") {
-            result.shouldBeFailure(
-                expectedValue
-            )
+        throwsAssertionError("Failure(reason=Test failed) should be Success(value=Test failed)") {
+            actualResult.shouldBeSuccess(actualValue)
         }
     }
 
-    private fun expectsAssertionError(message: String, block: () -> Unit) =
-        assertThrows<AssertionError> { block() }.also { assertThat(it.message, present(equalTo(message))) }
+    @Test
+    fun `should correctly assert value`() {
+        throwsAssertionError("Failure(reason=Actual value) should be Failure(reason=Expected value)") {
+            Failure("Actual value").shouldBeFailure("Expected value")
+        }
+
+        throwsAssertionError("Success(value=Actual value) should be Success(value=Expected value)") {
+            Success("Actual value").shouldBeSuccess("Expected value")
+        }
+    }
+
+    private fun throwsAssertionError(message: String, block: () -> Unit) =
+        assertThrows<AssertionError> { block() }.also {
+            assertThat(it.message, present(equalTo(message)))
+        }
+
 }
