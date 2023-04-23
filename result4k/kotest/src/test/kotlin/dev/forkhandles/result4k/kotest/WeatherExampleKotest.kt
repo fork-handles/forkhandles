7bc40d73996e64ba0baaa99b4a654ec9459a072b
Andrew O'Hara <ohara.andrew314@gmail.com>
2023-04-23 13:28:42 -0400

    add new result4k example, and documentation for kotest and hamkrest

---
+++ result4k/kotest/src/test/kotlin/dev/forkhandles/result4k/kotest/WeatherExampleKotest.kt
@@ -0,0 +1,33 @@
+package dev.forkhandles.result4k.kotest
+
+import dev.forkhandles.result4k.Weather
+import dev.forkhandles.result4k.WeatherError
+import dev.forkhandles.result4k.getWeather
+import io.kotest.matchers.ints.shouldBeGreaterThan
+import io.kotest.matchers.ints.shouldBeInRange
+import org.junit.jupiter.api.Test
+import java.math.BigDecimal
+
+class WeatherExampleKotest {
+    @Test
+    fun `assert any success`() = getWeather(30).shouldBeSuccess()
+
+    @Test
+    fun `assert exact success`() = getWeather(20) shouldBeSuccess Weather(BigDecimal("295.15"), 101_390)
+
+    @Test
+    fun `assert success block`() = getWeather(10) shouldBeSuccess { weather ->
+        weather.pascals shouldBeGreaterThan 100_000
+    }
+
+    @Test
+    fun `assert any failure`() = getWeather(9001).shouldBeFailure()
+
+    @Test
+    fun `assert exact failure`() = getWeather(9001) shouldBeFailure WeatherError(404, "unsupported location")
+
+    @Test
+    fun `assert failure block`() = getWeather(9001) shouldBeFailure { error ->
+        error.code shouldBeInRange 400..499
+    }
+}
