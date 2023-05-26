5a061643a183b3fc1f7dfb603a93c50f496b6af6
marcusdunn <marcus.s.dunn@gmail.com>
2023-05-26 10:19:18 -0700

    added an overload for `isFailure` and `isSuccess` that runs a matcher on the inner `value` or reason.

--- result4k/hamkrest/src/main/kotlin/dev/forkhandles/result4k/hamkrest/matchers.kt
+++ result4k/hamkrest/src/main/kotlin/dev/forkhandles/result4k/hamkrest/matchers.kt
@@ -2,31 +2,33 @@ package dev.forkhandles.result4k.hamkrest
 
 import com.natpryce.hamkrest.MatchResult
 import com.natpryce.hamkrest.Matcher
-import com.natpryce.hamkrest.describe
+import com.natpryce.hamkrest.equalTo
+import com.natpryce.hamkrest.has
 import dev.forkhandles.result4k.Failure
 import dev.forkhandles.result4k.Result4k
 import dev.forkhandles.result4k.Success
-import kotlin.reflect.KClass
 
-fun <T> isSuccess(expected: T): Matcher<Result4k<T, *>> = matchValue(Success(expected))
-fun isSuccess(): Matcher<Result4k<*, *>> = matchType(Success::class)
+fun <T> isSuccess(expected: T): Matcher<Result4k<T, *>> = isSuccess(equalTo(expected))
+fun <T> isSuccess(matcher: Matcher<T>): Matcher<Result4k<T, *>> =
+    isA<Success<T>>(has("value", Success<T>::value, matcher))
+fun <T> isSuccess(): Matcher<Result4k<T, *>> = isA<Success<T>>()
+fun <E> isFailure(expected: E): Matcher<Result4k<*, E>> = isFailure(equalTo(expected))
+fun <E> isFailure(matcher: Matcher<E>): Matcher<Result4k<*, E>> =
+    isA<Failure<E>>(has("reason", Failure<E>::reason, matcher))
 
-fun <E> isFailure(expected: E): Matcher<Result4k<*, E>> = matchValue(Failure(expected))
-fun isFailure(): Matcher<Result4k<*, *>> = matchType(Failure::class)
+fun <E> isFailure(): Matcher<Result4k<*, E>> = isA<Failure<E>>()
 
-private fun matchValue(expected: Result4k<*, *>) = object : Matcher<Result4k<*, *>> {
-    override fun invoke(actual: Result4k<*, *>) = match(actual == expected) { "was: ${describe(actual)}" }
-    override val description: String get() = "is ${describe(expected)}"
-    override val negatedDescription: String get() = "is not ${describe(expected)}"
-}
+// same as hamkrest's isA, but only prints the simple name of the class instead of the fully qualified name
+private inline fun <reified T : Result4k<*, *>> isA(downcastMatcher: Matcher<T>? = null) = object : Matcher<Any> {
+    override fun invoke(actual: Any) =
+        if (actual !is T) {
+            MatchResult.Mismatch("was: $actual")
+        } else if (downcastMatcher == null) {
+            MatchResult.Match
+        } else {
+            downcastMatcher(actual)
+        }
 
-private fun <T : Result4k<*, *>> matchType(expected: KClass<T>) = object : Matcher<Result4k<*, *>> {
-    override fun invoke(actual: Result4k<*, *>) = match(expected.isInstance(actual)) { "was: ${describe(actual)}" }
-    override val description: String get() = "is ${expected.simpleName}"
-    override val negatedDescription: String get() = "is not ${expected.simpleName}"
-}
-
-private inline fun match(comparison: Boolean, describeMismatch: () -> String): MatchResult = when {
-    comparison -> MatchResult.Match
-    else -> MatchResult.Mismatch(describeMismatch())
+    override val description: String
+        get() = "is a ${T::class.simpleName}" + if (downcastMatcher == null) "" else " and ${downcastMatcher.description}"
 }
