c306ab4458aae3666222de88359569b58cb75c6b
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2023-07-23 12:07:16 +0100

    use backticks in kdocs

--- result4k/core/src/main/kotlin/dev/forkhandles/result4k/nullables.kt
+++ result4k/core/src/main/kotlin/dev/forkhandles/result4k/nullables.kt
@@ -1,25 +1,25 @@
 package dev.forkhandles.result4k
 
 /*
- * Translate between the Result and Nullable/Optional/Maybe monads
+ * Translate between the `Result` and Nullable/Optional/Maybe monads
  */
 
 /**
- * Convert a nullable value to a Result, using the result of [failureDescription] as the failure reason
- * if the value is null.
+ * Convert a nullable value to a `Result`, using the result of [failureDescription] as the failure reason
+ * if the value is `null`.
  */
 inline fun <T, E> T?.asResultOr(failureDescription: () -> E) =
     if (this != null) Success(this) else Failure(failureDescription())
 
 /**
- * Convert a Success of a nullable value to a Success of a non-null value or a Failure,
- * using the result of [failureDescription] as the failure reason, if the value is null.
+ * Convert a `Success` of a nullable value to a `Success` of a non-null value or a `Failure`,
+ * using the result of [failureDescription] as the failure reason, if the value is `null`.
  */
 inline fun <T : Any, E> Result<T?, E>.filterNotNull(failureDescription: () -> E) =
     flatMap { it.asResultOr(failureDescription) }
 
 /**
- * Returns the success value, or null if the Result is a failure.
+ * Returns the success value, or `null` if the `Result` is a failure.
  */
 fun <T, E> Result<T, E>.valueOrNull() = when (this) {
     is Success<T> -> value
@@ -27,7 +27,7 @@ fun <T, E> Result<T, E>.valueOrNull() = when (this) {
 }
 
 /**
- * Returns the failure reason, or null if the Result is a success.
+ * Returns the failure reason, or `null` if the `Result` is a success.
  */
 fun <T, E> Result<T, E>.failureOrNull() = when (this) {
     is Success<T> -> null
