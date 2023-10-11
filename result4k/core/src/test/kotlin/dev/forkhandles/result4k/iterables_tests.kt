98c507d87d692231d17a60434e8e7ad4a2e9d255
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2023-10-11 20:25:00 +0100

    refactoring AllValuesTests, AnyValuesTests, PartitionTests

--- result4k/core/src/test/kotlin/dev/forkhandles/result4k/iterables_tests.kt
+++ result4k/core/src/test/kotlin/dev/forkhandles/result4k/iterables_tests.kt
@@ -4,45 +4,42 @@ import org.junit.jupiter.api.Test
 import kotlin.random.Random
 import kotlin.test.assertEquals
 
-class AllValuesTests {
+abstract class AllValuesTests(
+    private val allValues: List<Result<Int, String>>.() -> Result<List<Int>, String>
+) {
     @Test
-    fun `returns values of results, if all are success`() {
+    fun `returns values of all results or the first failure`() {
+        assertEquals(
+            Success(emptyList<Int>()),
+            emptyList<Result<Int, String>>().allValues()
+        )
         assertEquals(
             Success(listOf(1, 2, 3)),
             listOf(Success(1), Success(2), Success(3)).allValues()
         )
-    }
-
-    @Test
-    fun `returns first failure encountered, if not all are success`() {
         assertEquals(
             Failure("bad"),
             listOf(Success(1), Failure("bad"), Success(3)).allValues()
         )
-    }
-}
-
-class AllValuesSequenceTests {
-    @Test
-    fun `returns values of results, if all are success`() {
-        assertEquals(
-            Success(listOf(1, 2, 3)),
-            sequenceOf(Success(1), Success(2), Success(3)).allValues()
-        )
-    }
-
-    @Test
-    fun `returns first failure encountered, if not all are success`() {
         assertEquals(
             Failure("bad"),
-            sequenceOf(Success(1), Failure("bad"), Success(3)).allValues()
+            listOf(Success(1), Failure("bad"), Success(3), Failure("also bad")).allValues()
         )
     }
 }
 
-class AnyValuesTests {
+class AllValuesIterableTests : AllValuesTests({ allValues() })
+class AllValuesSequenceTests : AllValuesTests({ asSequence().allValues() })
+
+abstract class AnyValuesTests(
+    private val anyValues: List<Result<Int, String>>.() -> List<Int>
+) {
     @Test
     fun `returns any values of an iterable of results, dropping failures`() {
+        assertEquals(
+            emptyList(),
+            emptyList<Result<Int, String>>().anyValues()
+        )
         assertEquals(
             listOf(1, 2, 3),
             listOf(Success(1), Success(2), Success(3)).anyValues()
@@ -58,21 +55,17 @@ class AnyValuesTests {
     }
 }
 
-class AnyValuesSequenceTests {
-    @Test
-    fun `returns any values of an iterable of results, dropping failures`() {
-        assertEquals(listOf(1, 2, 3), sequenceOf(Success(1), Success(2), Success(3)).anyValues().toList())
-        assertEquals(listOf(1, 3), sequenceOf(Success(1), Failure("bad"), Success(3)).anyValues().toList())
-        assertEquals(emptyList(), sequenceOf(Failure("bad")).anyValues().toList())
-    }
-}
+class AnyValuesIterableTests : AnyValuesTests({ anyValues() })
+class AnyValuesSequenceTests : AnyValuesTests({ asSequence().anyValues().toList() })
 
-class PartitionTests {
+abstract class PartitionTests(
+    private val partition: List<Result<Int, String>>.() -> Pair<List<Int>, List<String>>
+) {
     @Test
     fun `returns values and failures in separate lists`() {
         assertEquals(
-            Pair(listOf(1, 3), listOf("bad", "also bad")),
-            listOf(Success(1), Failure("bad"), Success(3), Failure("also bad")).partition()
+            Pair(emptyList(), emptyList()),
+            emptyList<Result<Int, String>>().partition()
         )
         assertEquals(
             Pair(listOf(1, 2, 3), emptyList()),
@@ -82,26 +75,15 @@ class PartitionTests {
             Pair(emptyList(), listOf("bad", "also bad")),
             listOf(Failure("bad"), Failure("also bad")).partition()
         )
+        assertEquals(
+            Pair(listOf(1, 3), listOf("bad", "also bad")),
+            listOf(Success(1), Failure("bad"), Success(3), Failure("also bad")).partition()
+        )
     }
 }
 
-class PartitionSequenceTests {
-    @Test
-    fun `returns values and failures in separate lists`() {
-        assertEquals(
-            Pair(listOf(1, 3), listOf("bad", "also bad")),
-            sequenceOf(Success(1), Failure("bad"), Success(3), Failure("also bad")).partition()
-        )
-        assertEquals(
-            Pair(listOf(1, 2, 3), emptyList()),
-            sequenceOf(Success(1), Success(2), Success(3)).partition()
-        )
-        assertEquals(
-            Pair(emptyList(), listOf("bad", "also bad")),
-            sequenceOf(Failure("bad"), Failure("also bad")).partition()
-        )
-    }
-}
+class PartitionIterableTests: PartitionTests({ partition() })
+class PartitionSequenceTests: PartitionTests({ asSequence().partition() })
 
 private fun randomPositiveInt() = Random.nextInt(1, 100)
 
