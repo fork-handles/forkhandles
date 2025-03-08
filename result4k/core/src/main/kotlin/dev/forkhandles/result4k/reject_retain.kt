108be1c46b01fba16cb0c8ef53955b9ef14c5dbd
Nat Pryce <sw@natpryce.com>
2025-03-08 12:11:08 +0000

    rejectIf and retainIf convenience operations with immediate tests

--- result4k/core/src/main/kotlin/dev/forkhandles/result4k/reject_retain.kt
+++ result4k/core/src/main/kotlin/dev/forkhandles/result4k/reject_retain.kt
@@ -1,8 +1,15 @@
 package dev.forkhandles.result4k
 
 
-inline fun <T, E> Result<T, E>.retainIf(test: (T) -> Boolean, otherwise: () -> E): Result<T,E> =
+inline fun <T, E> Result<T, E>.retainIf(test: (T) -> Boolean, otherwise: () -> E): Result<T, E> =
     flatMap { if (test(it)) Success(it) else Failure(otherwise()) }
 
-inline fun <T, E> Result<T, E>.rejectIf(test: (T) -> Boolean, error: () -> E): Result<T,E> =
-    retainIf({!test(it)}, error)
+inline fun <T, E> Result<T, E>.rejectIf(test: (T) -> Boolean, error: () -> E): Result<T, E> =
+    retainIf({ !test(it) }, error)
+
+
+inline fun <T, E> Result<T, E>.retainIf(b: Boolean, otherwise: () -> E): Result<T, E> =
+    flatMap { if (b) Success(it) else Failure(otherwise()) }
+
+inline fun <T, E> Result<T, E>.rejectIf(b: Boolean, error: () -> E): Result<T, E> =
+    retainIf(!b, error)
